583eb1d63980dace6ea14ab158ba51e86b14cf50
David Smith <david.smith@weaveability.com>
2019-12-30 14:48:25 +0000

    display author name

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
@@ -9,6 +10,10 @@ class CoursesPage extends Component {
   componentDidMount() {
     this.props.actions.loadCourses().catch(error => {
       alert("Loading courses failed" + error);
+    });
+
+    this.props.actions.loadAuthors().catch(error => {
+      alert("Loading authors failed" + error);
     });
   }
 
@@ -29,13 +34,25 @@ CoursesPage.propTypes = {
 
 function mapStateToProps(state) {
   return {
-    courses: state.courses
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+          }),
+    authors: state.authors
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch)
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
   };
 }
 
